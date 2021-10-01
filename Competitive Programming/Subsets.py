68541726c98e76a7054875714aa609e267429733
Ansel Francis Dsouza <53405646+ansel2000@users.noreply.github.com>
2021-10-01 19:51:11 +0530

    Update Subsets.py

--- Competitive Programming/Subsets.py
+++ Competitive Programming/Subsets.py
@@ -1,3 +1,6 @@
+# https://leetcode.com/problems/subsets/
+# The following is a easy python solution for the problem in the above link
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         output = [[]]
